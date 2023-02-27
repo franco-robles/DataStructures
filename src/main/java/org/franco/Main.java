089ff09773cae7079ab5563b0369b35a8718be84
Franco Robles <franco_robles94@outlook.com>
2023-02-27 20:59:46 -0300

    example

--- src/main/java/org/franco/Main.java
+++ src/main/java/org/franco/Main.java
@@ -1,5 +1,35 @@
-package org.franco;public class Main {
+package org.franco;
+
+import org.franco.linkedList.DoublyLinkedList;
+import org.franco.node.Node;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DoublyLinkedList<Integer> lista= new DoublyLinkedList<>();
+
+        lista.addEnd(1);
+        lista.addEnd(2);
+        lista.addEnd(3);
+        lista.addEnd(4);
+        lista.addEnd(5);
+        lista.deleteNode(lista.firstNode().next.next);
+        lista.beginning();
+        lista.addAfter(lista.firstNode().next, 9);
+        lista.addEnd(7);
+        lista.addFront(4);
+        lista.end();
+
+        Node<Integer> node = lista.firstNode();
+        while(node !=null){
+            System.out.print(node.value+"->next: ");
+            node = node.next;
+        }
+        System.out.println("");
+        Node<Integer> nodeLast = lista.lastNode();
+        while(nodeLast !=null){
+            System.out.print(nodeLast.value+"->prev: ");
+            nodeLast = nodeLast.previous;
+        }
+
     }
 }
